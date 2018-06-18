0728a06fea08a3fa19c21194fb3ccd7bcf51e3e3
AsherLMac <renpsybeta@gmail.com>
2018-06-18 18:00:20 -0400

    Chaining

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/plantr');
+const db = new Sequelize('postgres://localhost:5432/plantr', {logging: false});
 
 const Gardener = db.define('gardener', {
   name: {
--- seed.js
+++ seed.js
@@ -1,15 +1,23 @@
 const model = require('./models')
+const VegetablePlot = model.db.model('vegetable_plot')
 
-model.Vegetable.create({name: 'Carrot', color: 'orange', planted_on: new Date()})
-model.Vegetable.create({name: 'Broccoli', color: 'green', planted_on: new Date()})
-model.Vegetable.create({name: 'Beet', color: 'purple', planted_on: new Date()})
+model.Vegetable.create({ name: 'Carrot', color: 'orange', planted_on: new Date() })
+  .then((createdVeg) => {
+    return model.Gardener.create({ name: 'Bob', age: 56, favoriteVegetableId: createdVeg.id })
+      .then((createdGar) => {
+        return model.Plot.create({ size: 10, shaded: true, gardenerId: createdGar.id })
+      })
+  })
 
-model.db.sync()
-.then(() => {
-  console.log('Sync successful.');
-  model.db.close()
-})
-.catch(err => {
-  console.log('Error: ', err);
-  model.db.close()
-})
+model.Vegetable.create({ name: 'Broccoli', color: 'green', planted_on: new Date() })
+model.Vegetable.create({ name: 'Beet', color: 'purple', planted_on: new Date() })
+
+model.db.sync(/*{force: true}*/)
+  .then(() => {
+    console.log('Sync successful.');
+    model.db.close()
+  })
+  .catch(err => {
+    console.log('Error: ', err);
+    model.db.close()
+  })
